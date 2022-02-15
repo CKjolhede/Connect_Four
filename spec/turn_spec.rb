38cc89332665ad8e7c4719275a8824f23b30adc6
Chris Kjolhede <ckjolhede@gmail.com>
2022-02-15 11:56:47 -0700

    test: create test for return of attributes

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -12,4 +12,11 @@ RSpec.describe Turn do
     expect(turn).to be_a(Turn)
   end
 
+  it 'can return attributes' do
+    board = Board.new
+    winning_arrays = WinningArrays.new
+    turn = Turn.new(board, winning_arrays)
+    expect(turn.valid_input).to eq(%w(a b c d e f g))
+  end
+
 end
