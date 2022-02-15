fc736f6ba87bc76f5ec3fb620e4ee828b39e43c0
Chris Kjolhede <ckjolhede@gmail.com>
2022-02-15 12:22:35 -0700

    test: create test for interactions between turn and board || winning arrays class

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,5 +1,6 @@
 require './lib/board'
 require './lib/turn'
+require './lib/game'
 require './lib/winning_arrays'
 require 'rspec'
 
@@ -7,16 +8,32 @@ RSpec.describe Turn do
 
   it 'exists' do
     board = Board.new
-    winning_arrays = WinningArrays.new
+    winning_arrays = WinningArrays.new.array
     turn = Turn.new(board, winning_arrays)
     expect(turn).to be_a(Turn)
   end
 
   it 'can return attributes' do
     board = Board.new
-    winning_arrays = WinningArrays.new
+    winning_arrays = WinningArrays.new.array
     turn = Turn.new(board, winning_arrays)
     expect(turn.valid_input).to eq(%w(a b c d e f g))
+    expect(turn.full_column).to eq({'a' => 0, 'b' => 0, 'c' => 0, 'd' => 0, 'e' => 0, 'f' => 0, 'g' => 0})
   end
+end
+  describe "Turn user input hardcoded" do
+    #requires replacing gets to be changed to hardcoded 'a' on line 15 of turn.rb
+
+  it 'can interact with winning_arrays and board hash' do
+    board = Board.new
+    winning_arrays = WinningArrays.new.array
+    turn = Turn.new(board, winning_arrays)
+    turn.player
+    expect(winning_arrays[0][0]).to eq("X")
+    expect(board.hash[:a1]).to eq("X")
+  end
+
+
+
 
 end
